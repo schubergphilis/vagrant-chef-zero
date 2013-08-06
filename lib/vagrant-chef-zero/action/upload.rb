fbb2eaf593119b492b94ce304752d3667602afa4
Scott Smith <scott@boundary.com>
2013-08-06 01:43:18 -0700

    fix cookbook loading in select_items

--- lib/vagrant-chef-zero/action/upload.rb
+++ lib/vagrant-chef-zero/action/upload.rb
@@ -127,7 +127,15 @@ module VagrantPlugins
           elsif path.is_a?(Array)
             path
           elsif path.is_a?(String) && File.directory?(path)
+            opath = path
             path = Dir.glob("#{path}/*.json")
+            if path.empty?
+              Dir.glob("#{opath}/*").each do |p|
+                if File.directory?(p) && File.exists?("#{p}/metadata.rb")
+                  path << p
+                end
+              end
+            end
           elsif path.is_a?(String) && File.exists?(path)
             path = [path]
           else
