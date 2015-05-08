2397039e198915da28a7690947fec19cb2b90048
miguelaferreira <miguelferreira@me.com>
2015-05-08 11:20:38 +0200

    Explicitly enable the old rspec should syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,7 @@ require_relative "../lib/vagrant-chef-zero/server_helpers.rb"
 require_relative "../lib/vagrant-chef-zero.rb"
 
 require 'json'
+
+RSpec.configure do |c|
+  c.expect_with(:rspec) { |c| c.syntax = :should }
+end
