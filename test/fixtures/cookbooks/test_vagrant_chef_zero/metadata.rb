5b8a168b7da8f9fbbff40ac72fe66c0870e4249f
Miguel Ferreira <miguelferreira@me.com>
2016-01-05 13:02:19 +0100

    Add integration testing

---
+++ test/fixtures/cookbooks/test_vagrant_chef_zero/metadata.rb
@@ -0,0 +1,8 @@
+name             'test_vagrant_chef_zero'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures test_vagrant_chef_zero'
+long_description 'Installs/Configures test_vagrant_chef_zero'
+version          '0.1.0'
+
